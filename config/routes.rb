1a7f1d07b6c2e1db01cf7e0152725da777435618
Stephan Brown <stephan.r.brown@gmail.com>
2017-04-09 01:37:04 +0000

    Added UI for Stocks

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   get "welcome/index"
   get 'my_portfolio', to: "users#my_portfolio"
+  get 'search_stocks', to: "stocks#search"
 devise_scope :user do
   authenticated :user do
     root 'welcome#index', as: :authenticated_root
