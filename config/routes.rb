c26bc5ee73c4b49d3900ab67984a417fc19a5e49
Stephan Brown <stephan.r.brown@gmail.com>
2017-04-15 09:16:20 +0000

    Added links for friendships

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
    root 'users#my_portfolio'
   devise_for :users, :controllers => { :registrations => "user/registrations" }
   resources :user_stocks, except: [:show, :edit, :update]
+  resources :users, only: [:show]
+  resources :friendships
   get "welcome/index"
   get 'my_portfolio', to: "users#my_portfolio"
   get 'search_stocks', to: "stocks#search"
